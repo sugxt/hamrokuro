1e4bf21f9cfd7273fe75760782763ad56ccc9eda
Sugat Poudyal <102009003+sugxt@users.noreply.github.com>
2024-06-24 20:50:36 +0545

    changes

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,19 +1,32 @@
 "use client"
 import loginServices from "@/services/authServices/loginServices";
 import guffList from "@/services/guffServices/guffList";
+import { FormEvent, useState } from "react";
+
 
 export default function Login() {
 
 
-    const AuthCreds = {
-        username: "gatsu",
-        password:"00000000"
+    async function handleSubmit(e:FormEvent<HTMLFormElement>){
+        e.preventDefault()
+        const formData = new FormData(e.currentTarget)
+        const Credentials = {
+            username:formData.get("username"),
+            password:formData.get("password")
+        }
+        loginServices(Credentials)
+        console.log(Credentials)
     }
 
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
-            <button onClick={() => loginServices(AuthCreds)}>click</button>
-            <button onClick={()=>guffList()}>don't click</button>
+            <div>
+            <form onSubmit={handleSubmit} className="flex flex-col gap-2">
+                <input type="text" placeholder="username or email" name="username"/>
+                <input type="password" placeholder="password" name="password" />
+                <button type="submit">login</button>
+            </form>
+            </div>
         </main>
     );
 }
--- src/services/authServices/loginServices.tsx
+++ src/services/authServices/loginServices.tsx
@@ -3,13 +3,8 @@ import PocketBase from 'pocketbase'
 
 const pb = new PocketBase(process.env.PB_URL)
 
-interface AuthCreds {
-    username: string,
-    password: string,
-}
 
-
-const loginServices = async (Credentials:AuthCreds) => {
+const loginServices = async (Credentials:any) => {
 
     const authData = await pb.collection('guffadis').authWithPassword(Credentials.username, Credentials.password);
     console.log(authData)
