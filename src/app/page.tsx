d91276c093eaefeb13e186c3f08fa8a155236690
Sugat Poudyal <official.sugat@gmail.com>
2024-07-01 21:41:34 +0545

    Initialized global state for Posts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,4 @@
 import ProfileCard from "@/components/cards/ProfileCard";
-import Image from "next/image";
 export default function Home() {
   return (
     <section className="flex min-h-screen flex-col items-center justify-between p-24">
---
+++ src/store/usePostStore.tsx
@@ -0,0 +1,7 @@
+"use client"
+
+const usePostStore = () => {
+
+};
+
+export default usePostStore;
