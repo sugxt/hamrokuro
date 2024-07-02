29c1823021fb8573267c01b49f831783060de6b1
Sugat Poudyal <102009003+sugxt@users.noreply.github.com>
2024-07-02 18:14:55 +0545

    Functionality to Fetch Posts Added

--- src/services/guffServices/postList.tsx
+++ src/services/guffServices/postList.tsx
@@ -1,8 +1,12 @@
+"use client"
+import usePostStore from "@/store/usePostStore";
 import { postConstants } from "@/utils/db.constants";
 import { pb } from "@/utils/pocketbase";
-
-export default async function postList() {
+export default async function postList():Promise<void>{
+  const setPostData = usePostStore.getState().setPostData
+  pb.autoCancellation(false)
   const records = await pb.collection(postConstants.posts).getFullList({
     sort: "-created",
   });
+  setPostData(records)
 }
--- src/store/usePostStore.tsx
+++ src/store/usePostStore.tsx
@@ -1,7 +1,17 @@
-"use client"
+"use client";
+import { RecordModel } from "pocketbase";
+import { create } from "zustand";
 
-const usePostStore = () => {
+interface PostState {
+  postData: RecordModel[] | null;
+  setPostData: (data: RecordModel[]) => void;
+  clearAuthData: () => void;
+}
 
-};
+const usePostStore = create<PostState>((set) => ({
+  postData: null,
+  setPostData: (data) => set({postData:data}),
+  clearAuthData: () => set({ postData: null }),
+}));
 
 export default usePostStore;
