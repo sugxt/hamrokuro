ea7b8067e80c0f9e2a1f188b563a62479588cc55
Sugat Poudyal <102009003+sugxt@users.noreply.github.com>
2024-07-03 16:21:08 +0545

    Added a blocks folder to construct client components

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,28 +1,15 @@
-"use client";
 import PostCard from "@/components/cards/PostCard";
 import postList from "@/services/guffServices/postList";
 import usePostStore from "@/store/usePostStore";
 import { useEffect, useState } from "react";
+import type { Metadata } from "next";
+import ProfileBlock from "@/components/blocks/ProfileBlock";
+
+export const metadata: Metadata = {
+  title: "Home",
+  description: "Home Page to View Guffs and Gaffs",
+};
 
 export default function Page() {
-  const postData = usePostStore((state) => state.postData);
-  const setPostData = usePostStore((state) => state.setPostData);
-  useEffect(() => {
-    const fetchData = async () => {
-      await postList();
-    };
-    fetchData();
-    console.log(postData);
-  }, [setPostData]);
-
-  return (
-    <div className="w-full flex flex-col justify-center items-center pt-10">
-      <div className="flex flex-col gap-6 w-full justify-center items-center">
-        {postData &&
-          postData.map((post) => (
-            <PostCard key={post.id} data={post}/>
-          ))}
-      </div>
-    </div>
-  );
+  return <ProfileBlock />;
 }
---
+++ src/components/blocks/ProfileBlock.tsx
@@ -0,0 +1,27 @@
+"use client"
+import postList from "@/services/guffServices/postList";
+import usePostStore from "@/store/usePostStore";
+import React, { useEffect } from "react";
+import PostCard from "@/components/cards/PostCard"
+
+const ProfileBlock = () => {
+  const postData = usePostStore((state) => state.postData);
+  const setPostData = usePostStore((state) => state.setPostData);
+  useEffect(() => {
+    const fetchData = async () => {
+      await postList();
+    };
+    fetchData();
+    console.log(postData);
+  }, [setPostData]);
+  return (
+    <div className="w-full flex flex-col justify-center items-center pt-10">
+      <div className="flex flex-col gap-6 w-full justify-center items-center">
+        {postData &&
+          postData.map((post) => <PostCard key={post.id} data={post} />)}
+      </div>
+    </div>
+  );
+};
+
+export default ProfileBlock;
