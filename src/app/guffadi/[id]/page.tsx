2dbd4b5d9b067b962c3fb640d24c5ce59e320f61
Sugat Poudyal <102009003+sugxt@users.noreply.github.com>
2024-07-08 11:26:11 +0545

    Updated Dependencies and some minor design changes

--- src/app/guffadi/[id]/page.tsx
+++ src/app/guffadi/[id]/page.tsx
@@ -3,7 +3,7 @@ import UserProfileBlock from "@/components/blocks/UserProfileBlock";
 
 export default function Page({ params }: { params: { id: string } }) {
   return (
-    <div className="flex flex-col items-center pt-10 w-full justify-center gap-2">
+    <div className="flex flex-col items-center pt-10 w-full justify-center gap-10">
       <UserProfileBlock id={params.id} />
       <GuffadiGuffs id={params.id}/>
     </div>
--- src/components/blocks/UserProfileBlock.tsx
+++ src/components/blocks/UserProfileBlock.tsx
@@ -2,6 +2,7 @@
 import GuffadiServices from "@/services/guffadiServices/GuffadiServices";
 import { RecordModel } from "pocketbase";
 import React, { useEffect, useState } from "react";
+import { MdVerified } from "react-icons/md";
 
 const UserProfileBlock = ({ id }: { id: string }) => {
   const [isLoading, setIsLoading] = useState<boolean>(true);
@@ -31,9 +32,12 @@ const UserProfileBlock = ({ id }: { id: string }) => {
   }
 
   return (
-    <div>
-      <h1 className="text-lg font-semibold text-cyan-700">{userData.name}</h1>
-      <h1>@{userData.username}</h1>
+    <div className="profile-box flex flex-col items-start justify-start p-10 bg-neutral-100 rounded-xl w-96">
+      <div className="name-status flex flex-row gap-1 items-center">
+        <h1 className="text-lg font-semibold text-cyan-700">{userData.name}</h1>
+        {userData.verified && <MdVerified className="text-cyan-800 text-base" />}
+      </div>
+      <p className="text-neutral-600 text-sm">@{userData.username}</p>
     </div>
   );
 };
